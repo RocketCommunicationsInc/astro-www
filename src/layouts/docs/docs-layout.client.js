77e2c9a114e7252905c9a70724758cb60c86d39a
Jonathan Neal <jonathantneal@hotmail.com>
2022-08-03 13:39:33 -0400

    feat: add storybook preview

---
+++ src/layouts/docs/docs-layout.client.js
@@ -0,0 +1,22 @@
+const template = document.createElement('template')
+
+template.innerHTML = [
+	`<iframe scrolling="yes" src="#preview"></iframe>`,
+	`<ul>`,
+		`<li><a href="#storybook" target="_blank">Storybook</a></li>`,
+		`<li><a href="#github" target="_blank">Github</a></li>`,
+	`</ul>`,
+].join('')
+
+requestAnimationFrame(() => {
+	for (const el of document.querySelectorAll('.storybook-demo')) {
+		const style = getComputedStyle(el)
+		const content = template.content.cloneNode(true)
+
+		content.querySelector('[src="#preview"]').src = `https://beta-astro-components.netlify.app/iframe.html?id=${style.getPropertyValue('--StorybookId').trim()}&viewMode=story`
+		content.querySelector('[href="#storybook"]').href = `https://astro-components.netlify.app/?path=/story/${style.getPropertyValue('--GitHubId').trim()}`
+		content.querySelector('[href="#github"]').href = `https://github.com/RocketCommunicationsInc/astro/tree/main/packages/web-components/src/components/${style.getPropertyValue('--GitHubId').trim()}`
+
+		el.append(content)
+	}
+})
