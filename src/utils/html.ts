c11580d327e3d43b0468141f01b6215d8c24cb48
Kiley <110477068+kiley-mitti@users.noreply.github.com>
2022-10-24 17:14:09 -0400

    feature - homepage updates and carousel web component added
    * initial commit
    
    * explainers complete (?)
    
    * update events
    
    * more styling complete except I broke articles :(
    
    * small fix
    
    * Build a Slideshow with Jonathan!
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>
    
    * awesome carousel component with Jonathan!
    
    * articles completion
    
    * update footer images
    
    * update footer breakpoints
    
    * article image fixes
    
    * fix little css nits
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -1,5 +1,12 @@
 const range = new Range()
 
+/** Returns a DocumentFragment representing the given HTML markup. */
 export const html = range.createContextualFragment.bind(range) as (fragment: string) => DocumentFragment
 
+/** Returns a Node representing the first contents of the given HTML markup. */
+export const h = <T = HTMLElement>(fragment: string) => html(fragment).firstChild as T
+
 range.selectNodeContents(document.createElement('template'))
+
+/** Returns a number representing the maximum left scroll offset possible for the element. */
+export const getScrollLeftMax = (target: Element) => target.scrollWidth - target.clientWidth
