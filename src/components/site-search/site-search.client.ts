c1c5a093dd7129685b59fc5271004f4ded30987c
Frances Morrison <Frances.Morrison87@gmail.com>
2022-11-16 13:46:02 -0500

    (fix): Restore keyboard controls on search (#43)
    * Cmd+k focuses site search from anywhere in the site now.
    
    * Adding arrow navigation of search elements, enter key press and escape key press.
    
    * Fixing bug where event listeners were being doubled when results item were focused or keys pressed.
    
    * Fixing bugs where tabbing and arrows did not work in all browsers
    
    * Making tab and shift+tab work explicitly in all browsers, making cmd+k work in Firefox
    
    * Navigator platform
    
    * Redoing search keyboard commands yet again, this time to more closely match Algolia. The selecting of list items via arrows is actually a simulation of focus, not actual focus.
    
    * Small code clean-ups
    
    * Moving keyboard controls into its own event listener.
    
    * Resolving comments from Jonathan.
    
    * Update src/components/site-search/site-search.client.ts
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>
    
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>

--- src/components/site-search/site-search.client.ts
+++ src/components/site-search/site-search.client.ts
@@ -45,12 +45,15 @@ requestAnimationFrame(() => {
 			html: utils.html,
 		}
 	}
-
+	/** The modifier key of the platform ('Meta' for Apple devices, otherwise 'Control'). */
+	const modifierKey = /^(MacIntel|iPhone)$/.test(navigator.platform) ? 'metaKey' : 'ctrlKey'
 	let search: Promise<SearchUtils>
 	let searchFrame = 0
 	let searchForm = document.getElementById('search')!
 	let searchResults = document.getElementById('search-results')!
 	let searchElement = <HTMLInputElement>document.getElementById('search-field')!
+	let resultChildren: any[] | NodeListOf<Element> = []
+	const navigation = searchForm.closest('.p-navigation')!
 
 	if (searchForm) {
 		searchForm.addEventListener('submit', (event) => {
@@ -59,18 +62,22 @@ requestAnimationFrame(() => {
 	}
 
 	if (searchElement) {
+		document.addEventListener('keydown', (event) => {
+			if (event.key === 'k' && event[modifierKey]) {
+				event.preventDefault()
+				searchElement.focus()
+			}
+		})
 		searchElement.addEventListener('focus', (event) => {
 			search = search || createSearch()
 		})
 
 		searchElement.addEventListener('input', (event: InputEvent & { target: HTMLInputElement }) => {
 			cancelAnimationFrame(searchFrame)
-
 			search.then(searchUtils => {
 				searchFrame = requestAnimationFrame(async () => {
 					const results = await searchUtils.search(event.target.value)
 					const hasResults = Boolean(results && results.items.length)
-					const navigation = searchForm.closest('.p-navigation')!
 
 					searchForm.classList.toggle('-has-results', hasResults)
 					navigation.classList.toggle('-has-results', hasResults)
@@ -92,8 +99,59 @@ requestAnimationFrame(() => {
 					}
 
 					searchResults.replaceChildren(hdom)
+					resultChildren = searchResults.querySelectorAll('.listitem')
+					resultChildren.forEach((result, index) => {
+						if (index === 0) {
+							result.setAttribute('aria-selected', 'true')
+						} else {
+							result.setAttribute('aria-selected', 'false')
+						}
+					})
 				})
 			})
 		})
+		let indexNumber = 0
+		const onKeydown = (event: KeyboardEvent) => {
+			if (resultChildren.length > 0) {
+				if (event.key === 'ArrowDown') {
+					event.preventDefault()
+					if (indexNumber < (resultChildren.length - 1)) {
+						indexNumber++
+					}
+					resultChildren.forEach((result: HTMLElement, index: number) => {
+						if (index === indexNumber) {
+							result.setAttribute('aria-selected', 'true')
+						} else {
+							result.setAttribute('aria-selected', 'false')
+						}
+					})
+				} else if (event.key === 'ArrowUp') {
+					event.preventDefault()
+					if (indexNumber >= 1) {
+						indexNumber--
+					}
+					resultChildren.forEach((result: HTMLElement, index: number) => {
+						if (index === indexNumber) {
+							result.setAttribute('aria-selected', 'true')
+						} else {
+							result.setAttribute('aria-selected', 'false')
+						}
+					})
+				} else if (event.key === 'Enter') {
+					resultChildren.forEach((result: HTMLElement) => {
+						if (result.getAttribute('aria-selected') === 'true') {
+							result.querySelector('a')?.click()
+						}
+					})
+				} else if (event.key === 'Escape') {
+					// clears the search control and removes search results
+					searchElement.value = ''
+					searchResults.replaceChildren()
+					searchForm.classList.remove('-has-results')
+					navigation.classList.remove('-has-results')
+				}
+			}
+		}
+		searchElement.addEventListener('keydown', onKeydown)
 	}
 })
