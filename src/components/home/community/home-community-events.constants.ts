4e274b04c7a5dc05bac6c5404080f71ab9c8a2ff
Kiley Mitti <kiley.mitti@rocketcom.com>
2022-12-09 09:27:36 -0500

    fix for server side too

--- src/components/home/community/home-community-events.constants.ts
+++ src/components/home/community/home-community-events.constants.ts
@@ -10,7 +10,7 @@ export const fetchGoogleCalendarEvents = async () => {
 		maxResults: '6',
 		orderBy: 'startTime',
 		singleEvents: 'true',
-		startDate: new Date().toISOString(),
+		minTime: new Date().toISOString(),
 	})
 
 	/** Request URL */
