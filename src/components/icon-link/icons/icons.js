e5594b349e615ad0c2a839925d9f57df99e4b289
Jonathan Neal <jonathantneal@hotmail.com>
2022-12-16 14:11:20 -0500

    WIP

---
+++ src/components/icon-link/icons/icons.js
@@ -0,0 +1,9 @@
+import figma from './figma.svg?raw'
+import github from './github.svg?raw'
+import storybook from './storybook.svg?raw'
+
+export default {
+	figma: figma.trim(),
+	github: github.trim(),
+	storybook: storybook.trim(),
+}
