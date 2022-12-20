0cf1541784092c733a2ae89455d2760f35945a28
Jonathan Neal <jonathantneal@hotmail.com>
2022-12-19 20:30:06 -0500

    WIP

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -7,12 +7,15 @@ const cloneRange = new Range()
 export const html = templateRange.createContextualFragment.bind(templateRange) as (fragment: string) => DocumentFragment
 
 /** Returns a Node representing the first contents of the given HTML markup. */
-export const h = <T = HTMLElement>(fragment: string) => html(fragment).firstChild as T
+export const h = <T extends HTMLElement>(fragment: string) => html(fragment).firstChild as T
 
-export const c = <T = HTMLElement>(container: T, ...nodes: (Node | string)[]) => (
-	(container as ParentNode).replaceChildren(...nodes),
-	container
-)
+export const c = <T extends ParentNode>(
+	container: T,
+	...nodes: (Node | string)[]
+) => {
+	container.replaceChildren(...nodes)
+	return container
+}
 
 export const cloneContents = (target: Node) => (
 	cloneRange.selectNodeContents(target),
