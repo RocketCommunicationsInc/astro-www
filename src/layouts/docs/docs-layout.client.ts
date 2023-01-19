6561bfa3955a3f44cd0a54051bdaea34c84c9cd8
Jonathan Neal <jonathantneal@hotmail.com>
2023-01-18 23:17:47 -0500

    organize site functionality
    - Add client scripting for layouts.
    - Move Google Analytics into client utils.
    - Support a global clipboard:write event for writing to the clipboard.
    - Support a global download:file event to downloading files.
    - Move icons.svg.ts to icon-library.svg.ts to match page route.
    - Fix an issue where icon-library produced unnecessary commas.
    - Fix an issue where icon-library produced unnecessary commas.

---
+++ src/layouts/docs/docs-layout.client.ts
@@ -0,0 +1,3 @@
+import 'project:utils/client/handle-external-links'
+import 'project:utils/client/handle-button-emits'
+import 'project:utils/client/handle-clipboard-writes'
---
+++ src/layouts/new-docs/new-docs-layout.client.ts
@@ -0,0 +1,4 @@
+import 'project:utils/client/handle-external-links'
+import 'project:utils/client/handle-button-emits'
+import 'project:utils/client/handle-clipboard-writes'
+import 'project:utils/client/handle-download-files'
