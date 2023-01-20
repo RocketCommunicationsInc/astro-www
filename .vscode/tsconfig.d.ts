556d1d17599c0eb406678c8c487a7a0cc17fd80b
Jonathan Neal <jonathantneal@hotmail.com>
2023-01-20 17:22:21 -0500

    nit: remove d.ts comment

--- .vscode/tsconfig.d.ts
+++ .vscode/tsconfig.d.ts
@@ -12,10 +12,6 @@ interface CSSStyleDeclaration {
 	setProperty(property: string, value: string | number | null, priority?: string): void
 }
 
-// declare var setTimeout: {
-// 	(callback)
-// }
-
 interface SetTimeout {
 	<TArgs extends any[]>(callback: (...args: TArgs) => void, ms?: number, ...args: TArgs): number
 }
