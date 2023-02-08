b3d45fd22110824057d0182e5f00bdcbded764c9
Jonathan Neal <jonathantneal@hotmail.com>
2023-02-08 08:35:08 -0500

    Update Icon Library deep internals

--- src/pages/icon-library.svg.ts
+++ src/pages/icon-library.svg.ts
@@ -1,28 +1,21 @@
 import type { APIRoute } from 'astro'
 
 import icons from 'project:data/icons.json'
-
-const html = `<svg xmlns="http://www.w3.org/2000/svg">\n${
-	Object.entries(icons).map(
-		([ name, icons ]) => (
-			`\t<g data-name=${JSON.stringify(name)}>\n${
-				Object.entries(icons).map(
-					([ id, { name, html, tags }]) => (
-						`\t\t<symbol id="icon-${id}" viewBox="0 0 24 24">\n${
-							[
-								`\t\t\t<title>${name}</title>`,
-								`\t\t\t${html}`,
-								`\t\t\t<metadata>${tags.join(', ')}</metadata>`,
-							].join('\n')
-						}\n\t\t</symbol>\n`
-					)
-				).join('')
-			}</g>\n`
-		)
-	).join('')
-}</svg>`
+import { h } from 'project:utils/host/h'
 
 export const get: APIRoute = () => ({
-	body: html,
+	body: h.svg({ xmlns: 'http://www.w3.org/2000/svg' },
+		...Object.entries(icons).map(
+			([ name, icons ]) => h.g({ 'data-name': name },
+				...Object.entries(icons).map(
+					([ id, { name, path, tags }]) => h.symbol({ id: `icon-${id}`, viewBox: '0 0 24 24' },
+						h.title({}, name),
+						h.path(path),
+						h.metadata({}, tags.join(', '))
+					)
+				)
+			)
+		)
+	),
 	encoding: 'utf8',
 })
