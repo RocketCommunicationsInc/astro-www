329698cbd84c3a1efac529160438ba3d037753cf
Jonathan Neal <jonathantneal@hotmail.com>
2023-02-09 16:06:41 -0500

    Update GTag to use globalThis.dataLayer

--- src/utils/client/google-analytics.ts
+++ src/utils/client/google-analytics.ts
@@ -1,28 +1,21 @@
 export const id = 'G-ZHMMGPG3B3'
 
 const scriptURL = `https://www.googletagmanager.com/gtag/js?id=${id}`
-
-const dataLayer = (
-	globalThis.dataLayer = (
-		globalThis.dataLayer || [
-			[ 'js', new Date() ],
-			[ 'config', id ],
-		]
-	)
-) as [ command: string, ...parameters: any[]][ ]
-
-export const gtag = (
-	globalThis.gtag = (
-		globalThis.gtag || (
-			(...args: any) => dataLayer.push(args)
-		)
-	)
-) as {
-	(command: string, ...parameters: string[]): void
-}
-
 const scriptElement = document.querySelector(`script[src=${JSON.stringify(scriptURL)}]`) || Object.assign(document.createElement('script'), { src: scriptURL })
 
 if (!scriptElement.isConnected) {
 	document.head.append(scriptElement)
 }
+
+if (!globalThis.dataLayer) {
+	globalThis.dataLayer = [
+		[ 'js', new Date() ],
+		[ 'config', id ],
+	]
+}
+
+if (!globalThis.gtag) {
+	globalThis.gtag = (...args: any) => globalThis.dataLayer.push(args)
+}
+
+export const gtag = globalThis.gtag
