80cd4f5f85b826c96e99657025cba8dc15ef2667
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-02-17 10:56:01 -0500

    update layout in js version

--- src/components/home/community/home-community-events.update.js
+++ src/components/home/community/home-community-events.update.js
@@ -10,31 +10,35 @@ const createCalendarEventFragment = (
 	/** @type {CalendarEvent} */
 	event
 ) => withCalendarInteractiveBehavior(h(`<article class="p-community-event --closed">
-	<small class="p-community-event-date">
-	<span class="-date">${event.start.date
-		? getDate(event.start.date)
-		: new Date(event.start.dateTime).toLocaleString('en-US', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		})
-	}</span>
-	${
-		event.start.dateTime
-			? `<span class="-time">${
-				new Date(event.start.dateTime).toLocaleString('en-US', {
-					hour: 'numeric',
-					minute: '2-digit',
-					timeZoneName: 'short'
-				})
-		}</span>`
-		: toString(event.start.date && `<span class="-time">All Day!</span>`)
-	}
-	</small>
 	<hgroup class="p-community-event-heading">
 		<h5>${event.summary}</h5>
-		${toString(event.location && `<small class="p-community-event-subheading">${event.location}</small>`)}
 	</hgroup>
+	<span class="p-community-event-info"> 
+		<small class="p-community-event-date">
+		<span class="-date">${event.start.date
+			? getDate(event.start.date)
+			: new Date(event.start.dateTime).toLocaleString('en-US', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+			})
+		}</span>
+		${
+			event.start.dateTime
+				? `<span class="-time">${
+					new Date(event.start.dateTime).toLocaleString('en-US', {
+						hour: 'numeric',
+						minute: '2-digit',
+						timeZoneName: 'short'
+					})
+			}</span>`
+			: toString(event.start.date && `<span class="-time">All Day!</span>`)
+		}
+	</small>
+	<span class='-second-col'>
+		${toString(event.location && `<span class="p-community-event-subheading">${event.location}</span>`)}
+	</span>
+		
 	<span class="p-community-event-actions">
 		<button>View Details</button>
 	</span>
