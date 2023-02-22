84f90fa4061b3aa27558f415a5f21a1b29c80736
Frances Morrison <Frances.Morrison87@gmail.com>
2023-02-22 09:29:04 -0500

    feat(icon library v3): New search bar with categories, redone icon side-panel (#132)
    * making changes to icon library
    
    * WIP push so frances can take over
    
    Co-authored-by: Frances Morrison <Frances.Morrison87@gmail.com>
    
    * Working on positioning of icon-aside, icon grid, search bar
    
    * Working on side-panel and categories appearing in search as you scroll
    
    * Working on search and side panel
    
    * re-positioning of icon panel and functions to keep it in place.
    
    * Fixing spacing and adjusting breakpoints
    
    * Setting max-width on content and redoing search bar with a toolbar slot in new-docs-layout
    
    * Adding in 'icons not found' page / state
    
    * Making the search 500px max-width
    
    * Adjusting icon size, border-radius on search and group header font size.
    
    * patch: Using case on search would cause the icon to not be found.
    
    * Adding in scroll back to top when search results appear.
    
    * Updating function to scroll back to top on search to exclude header.
    
    * fix: on first load before any button press icon side panel flashes above header for a moment when opened.
    
    * Fixing positioning on autoscroll using dropdown so the sticky icon bar is not covering icons.
    
    * Fixing new issue where the dropdown was not highlighting the right name.
    
    * Fixing the icon headers appearing appropriately, not using magic numbers
    
    * Removing magic number for scroll offset on category dropdown choice, adding resize event listener to position icon side-panel.
    
    * Fixing category select so focus ring wasn't pressed against the category name.
    
    * Fixing 1440 breakpoint
    
    * Update src/components/icon-search/icon-search.astro
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>
    
    * Update src/components/icon-search/icon-search.astro
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>
    
    * Update src/components/icon-search/parts/search-categories.css
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>
    
    * Final nits
    
    * No results page broke, fixing.
    
    ---------
    
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    Co-authored-by: Kiley Mitti <kiley.mitti@rocketcom.com>
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>

--- src/components/icon-search/icon-search.client.ts
+++ src/components/icon-search/icon-search.client.ts
@@ -1,83 +1,59 @@
-// const iconPanel = document.querySelector<HTMLElement>('icon-panel')!
+const icons = <NodeList>document.querySelectorAll('.icon')
+const nav = <HTMLElement>document.querySelector('.p-navigation')
+const pageHeader = <HTMLElement>document.querySelector('.page-header')
+const iconSearch = <HTMLElement>document.querySelector('.p-icon-search')
+const sidePanel = <HTMLElement>document.querySelector('icon-panel')
 
-// const iconGroups = [
-// 	...document.querySelectorAll('.p-icon-groups .-group')!
-// ].map(
-// 	element => {
-// 		const name = element.querySelector('.-group-heading')?.textContent!.toLowerCase()!
+// add click event listener to bump page down if the icon panel is open
+icons.forEach(icon => {
+	icon.addEventListener('click', () => {
+		let pageHeaderHeight: number = pageHeader.offsetHeight
+		let navHeight: number = window.visualViewport.width < 800 ? nav.offsetHeight : 0
+		let heightToBottomOfSearchPanel: number = pageHeaderHeight + navHeight
 
-// 		const icons = [
-// 			...element.querySelectorAll('.icon')
-// 		].map(
-// 			element => {
-// 				const name = element.querySelector('figcaption')?.textContent!.toLowerCase()!
+		let searchRectY: number = iconSearch?.getBoundingClientRect().top
 
-// 				return {
-// 					name,
-// 					element,
-// 				}
-// 			}
-// 		)
+		if (searchRectY! > 0) {
+			setIconPanelPosition()
+			window.scrollTo(0, heightToBottomOfSearchPanel)
+		}
+	})
+})
 
-// 		return {
-// 			name,
-// 			element,
-// 			icons
-// 		}
-// 	}
-// )
+// sets icon panel distance from top dynamically so it always sits underneath the icon search even though it is position: fixed
+// to be run on scroll
+const setIconPanelPosition = () => {
+	let iconSearchHeight: number = iconSearch.offsetHeight
+	let navHeight: number = window.visualViewport.width < 800 ? nav.offsetHeight : 0
+	let pageHeaderHeight: number = pageHeader.offsetHeight
+	let pageHeaderPlusNavHeight: number = pageHeaderHeight + navHeight
+	let searchRect: number = iconSearch?.getBoundingClientRect().y
+	let offset: number = searchRect + iconSearchHeight
+  if (window.visualViewport.pageTop > pageHeaderPlusNavHeight) {
+	sidePanel.style.insetBlockStart = `${iconSearchHeight}px`
+  } else {
+    sidePanel.style.insetBlockStart = `${offset}px`
+  }
+}
 
-// const searchControl = document.querySelector<HTMLInputElement>('.icon-search-form .-control')!
-// const searchResultCountEl = document.querySelector<HTMLParagraphElement>('.p-icon-results')!
+// set intersection observer on header so the onscroll event listener is only running for the length of the header scroll
+const intersectionOptions = {
+	rootMargin: '0px',
+	threshold: 0,
+}
 
+const callback = (entries: any[]) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			window.addEventListener('scroll', setIconPanelPosition)
+		} else {
+			window.removeEventListener('scroll', setIconPanelPosition)
+		}
+	})
+}
 
+const observer = new IntersectionObserver(callback, intersectionOptions)
 
-// searchControl.addEventListener('input', () => {
-// 	let { value } = searchControl
-// 	let searchResultCount = 0
+observer.observe(pageHeader)
 
-// 	value = value.trim().toLowerCase()
-
-// 	iconPanel.removeAttribute('use')
-
-// 	for (let iconGroup of iconGroups) {
-// 		let nomatches = true
-// 		let earlymatch = !value || iconGroup.name.includes(value)
-
-// 		for (let icon of iconGroup.icons) {
-// 			const nomatch = !earlymatch && Boolean(value) && !icon.name.includes(value)
-
-// 			nomatches = nomatches && nomatch
-
-// 			icon.element.classList.toggle('nomatch', nomatch)
-
-// 			if (!nomatch) {
-// 				++searchResultCount
-// 			}
-// 		}
-
-// 		iconGroup.element.classList.toggle('nomatch', nomatches)
-
-// 		searchResultCountEl.textContent = (
-// 			searchResultCount
-// 				? (
-// 					searchResultCount === Number(searchResultCountEl.dataset.maxSize)
-// 				)
-// 					? `Showing all ${searchResultCount} icons.`
-// 				: `Showing ${searchResultCount} matching icons.`
-// 			: `No matching icons.`
-// 		)
-// 	}
-// })
-
-// addEventListener('focus', event => {
-// 	const icon = event.target as HTMLElement
-
-// 	if (icon.matches?.('figure.icon')) {
-// 		const use = icon.querySelector<SVGUseElement>('use[href]')!
-
-// 		const iconID = use.href.baseVal
-
-// 		iconPanel.setAttribute('use', iconID)
-// 	}
-// }, true)
+window.addEventListener('resize', setIconPanelPosition)
