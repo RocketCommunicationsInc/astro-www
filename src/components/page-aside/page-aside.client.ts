4006080e58463cefa71bcd0e5c77cb80c2529264
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-04-03 11:20:10 -0400

    Changing the in-page side nav so that only H2s are reflected

--- src/components/page-aside/page-aside.client.ts
+++ src/components/page-aside/page-aside.client.ts
@@ -1,76 +1,76 @@
 import { h } from 'project:utils/html.js'
 
-const createTableOfContentsNavigation = (headings: NodeListOf<HTMLHeadingElement>) => {
-	let currentHeading: HTMLHeadingElement | null
+// const createTableOfContentsNavigation = (headings: NodeListOf<HTMLHeadingElement>) => {
+// 	let currentHeading: HTMLHeadingElement | null
 
-	let headingObserver: IntersectionObserver | undefined
+// 	let headingObserver: IntersectionObserver | undefined
 
-	const asideLinkMap = new WeakMap<HTMLHeadingElement, HTMLAnchorElement>()
+// 	const asideLinkMap = new WeakMap<HTMLHeadingElement, HTMLAnchorElement>()
 
-	const aside = document.querySelector('.page-content-side-col .-content')!
-	const navElement = h<HTMLElement>('<nav class="p-inpage-navigation">')
-	const listElement = h<HTMLUListElement>('<ul>')
+// 	const aside = document.querySelector('.page-content-side-col .-content')!
+// 	const navElement = h<HTMLElement>('<nav class="p-inpage-navigation">')
+// 	const listElement = h<HTMLUListElement>('<ul>')
 
-	for (const heading of headings) {
-		const listItemElement = h<HTMLLIElement>('<li>')
-		const linkElement = h<HTMLAnchorElement>(`<a href="#${heading.id}">${heading.textContent}`)
+// 	for (const heading of headings) {
+// 		const listItemElement = h<HTMLLIElement>('<li>')
+// 		const linkElement = h<HTMLAnchorElement>(`<a href="#${heading.id}">${heading.textContent}`)
 
-		listItemElement.append(linkElement)
-		listElement.append(listItemElement)
+// 		listItemElement.append(linkElement)
+// 		listElement.append(listItemElement)
 
-		asideLinkMap.set(heading, linkElement)
-	}
+// 		asideLinkMap.set(heading, linkElement)
+// 	}
 
-	aside.append(navElement)
+// 	aside.append(navElement)
 
-	const visualViewport = globalThis.visualViewport!
+// 	const visualViewport = globalThis.visualViewport!
 
-	const onresize = () => {
-		if (headingObserver) {
-			headingObserver.disconnect()
-		}
+// 	const onresize = () => {
+// 		if (headingObserver) {
+// 			headingObserver.disconnect()
+// 		}
 
-		headingObserver = new IntersectionObserver((entries) => {
-			currentHeading = null
+// 		headingObserver = new IntersectionObserver((entries) => {
+// 			currentHeading = null
 
-			for (const entry of entries) {
-				if (entry.isIntersecting) {
-					currentHeading = entry.target as HTMLHeadingElement
+// 			for (const entry of entries) {
+// 				if (entry.isIntersecting) {
+// 					currentHeading = entry.target as HTMLHeadingElement
 
-					break
-				}
-			}
+// 					break
+// 				}
+// 			}
 
-			if (currentHeading) {
-				const { y } = listElement.getBoundingClientRect()
+// 			if (currentHeading) {
+// 				const { y } = listElement.getBoundingClientRect()
 
-				for (let heading of headings) {
-					const link = asideLinkMap.get(heading as HTMLHeadingElement)!
+// 				for (let heading of headings) {
+// 					const link = asideLinkMap.get(heading as HTMLHeadingElement)!
 
-					if (heading === currentHeading) {
-						const linkBox = link.getBoundingClientRect()
+// 					if (heading === currentHeading) {
+// 						const linkBox = link.getBoundingClientRect()
 
-						navElement.style.setProperty('--height', `${linkBox.height}px`)
-						navElement.style.setProperty('--offset', `${linkBox.y - y}px`)
-					}
+// 						navElement.style.setProperty('--height', `${linkBox.height}px`)
+// 						navElement.style.setProperty('--offset', `${linkBox.y - y}px`)
+// 					}
 
-					link.classList.toggle('current', heading === currentHeading)
-				}
-			}
-		}, {
-			rootMargin: `0% 0px -${visualViewport.height - 60}px`,
-			threshold: 0,
-		})
+// 					link.classList.toggle('current', heading === currentHeading)
+// 				}
+// 			}
+// 		}, {
+// 			rootMargin: `0% 0px -${visualViewport.height - 60}px`,
+// 			threshold: 0,
+// 		})
 
-		for (const heading of headings) {
-			headingObserver.observe(heading)
-		}
-	}
+// 		for (const heading of headings) {
+// 			headingObserver.observe(heading)
+// 		}
+// 	}
 
-	visualViewport.addEventListener('resize', onresize, { passive: true })
+// 	visualViewport.addEventListener('resize', onresize, { passive: true })
 
-	onresize()
-}
+// 	onresize()
+// }
 
 
 const createHeadingObserver = (headings: NodeListOf<HTMLHeadingElement>) => {
@@ -117,5 +117,5 @@ const createHeadingObserver = (headings: NodeListOf<HTMLHeadingElement>) => {
 	onresize()
 }
 
-createHeadingObserver(document.querySelectorAll('main [id]:is(h1,h2,h3,h4,h5,h6)'))
+createHeadingObserver(document.querySelectorAll('main [id]:is(h2)'))
 // createTableOfContentsNavigation(document.querySelectorAll('main [id]:is(h1,h2,h3,h4,h5,h6)'))
