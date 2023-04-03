d31170f96ec0fdd8eab5bd67621521d23424b8ec
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-04-03 13:00:47 -0400

    handling submittable states in the UI, mapping thumbs buttons to hidden radios.

--- src/components/feedback-widget/feedback-widget.client.ts
+++ src/components/feedback-widget/feedback-widget.client.ts
@@ -2,7 +2,37 @@ const rateButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.w
 const submitButton: HTMLButtonElement = document.querySelector('.widget_primary-button')!
 const emailInput: HTMLInputElement = document.querySelector('input[type="email"]')!
 const textarea: HTMLTextAreaElement = document.querySelector('textarea')!
-let formPopulated: boolean = false
+const buttonThumbsUpRadio: HTMLInputElement = document.querySelector('#button_thumbs-up')!
+const buttonThumbsDownRadio: HTMLInputElement = document.querySelector('#button_thumbs-down')!
+const hiddenThumbsUpRadio: HTMLInputElement = document.querySelector('#radio_thumbs-up')!
+const hiddenThumbsDownRadio: HTMLInputElement = document.querySelector('#radio_thumbs-down')!
+let emailPopulated: boolean = false
+let textareaPopulated: boolean = false
+let rateButtonSelected: boolean = false
+
+const handleRateButtonUncheckAll = () => {
+	// deselect UI buttons
+	for (const button of rateButtons) {
+		button.classList.remove('selected')
+	}
+
+	// pull checked state from both hidden radios
+	hiddenThumbsUpRadio.removeAttribute('checked');
+	hiddenThumbsDownRadio.removeAttribute('checked');
+}
+
+const handleRateButtonCheck = (button: HTMLButtonElement) => {
+	button.classList.add('selected')
+	// map selected button state to hidden radios for form submit
+	// add checked state to selected radio
+	if (button.id === 'button_thumbs-up') {
+		button.classList.contains('selected') ? hiddenThumbsUpRadio.setAttribute('checked', '') : hiddenThumbsUpRadio.removeAttribute('checked')
+	}
+
+	if (button.id === 'button_thumbs-down') {
+		button.classList.contains('selected') ? hiddenThumbsDownRadio.setAttribute('checked', '') : hiddenThumbsDownRadio.removeAttribute('checked')
+	}
+}
 
 const toggleWidget = () => {
 	const widgetWrapper = document.querySelector('.widget_wrapper')!
@@ -14,37 +44,46 @@ const toggleWidget = () => {
 	})
 	cancelButton.addEventListener('click', () => {
 		widgetWrapper?.classList.toggle('-active');
+		handleRateButtonUncheckAll();
 	})
-}
-
-const handleRateButtonRemove = () => {
-	for (const button of rateButtons) {
-		button.classList.remove('selected')
-	}
 }
 
 const isFormPopulated = () => {
 	emailInput.addEventListener('input', (event) => {
 		const target =  event.currentTarget as HTMLInputElement
-		target.value ? formPopulated = true : formPopulated = false
+		if (target.value) {
+			emailPopulated = true;
+			(!rateButtonSelected && !textareaPopulated) ? submitButton.disabled = true : submitButton.disabled = false
+		} else {
+			emailPopulated = false
+			if (!rateButtonSelected && !textareaPopulated) submitButton.disabled = true
+		}
 	})
 	textarea.addEventListener('input', (event) => {
 		const target =  event.currentTarget as HTMLInputElement
-		target.value ? formPopulated = true : formPopulated = false
+		if (target.value) {
+			textareaPopulated = true
+			submitButton.disabled = false
+		} else {
+			textareaPopulated = false
+			if (!rateButtonSelected) submitButton.disabled = true
+		}
 	})
 }
 
 const handleRateButtonSelected = (button: HTMLButtonElement) => {
 	// if it's selected already only deselect itself (which will mean both will be deselected), disable form submit
 	if (button.classList.contains('selected')) {
-		button.classList.remove('selected')
-		if(!formPopulated) {
+		handleRateButtonUncheckAll()
+		rateButtonSelected = false
+		if(!textareaPopulated) {
 			submitButton.disabled = true
 		}
 	} else {
 		// Remove selected from all, then add it to clicked button, enable form submit
-		handleRateButtonRemove()
-		button.classList.add('selected')
+		handleRateButtonUncheckAll()
+		handleRateButtonCheck(button)
+		rateButtonSelected = true
 		submitButton.disabled = false
 	}
 }
@@ -53,6 +92,7 @@ const handleRateButtonsClick = () => {
 	// handle toggling the thumbs up/down buttons on and off, making sure they are mutually exclusive
 	for (const button of rateButtons) {
 		button.addEventListener('click', () => {
+			// handle UI selection of button
 			handleRateButtonSelected(button)
 		})
 	}
