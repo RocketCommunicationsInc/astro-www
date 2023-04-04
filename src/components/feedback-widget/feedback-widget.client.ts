d0936714ba8f4c2bf0f039f94038cb41a41a0a63
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-04-04 09:24:36 -0400

    Working on sticking the widget to the top of the footer.

--- src/components/feedback-widget/feedback-widget.client.ts
+++ src/components/feedback-widget/feedback-widget.client.ts
@@ -1,3 +1,6 @@
+const widgetWrapper: HTMLElement = document.querySelector('.widget_wrapper')!
+const topTab: HTMLElement = document.querySelector('.widget_top-tab')!
+const cancelButton: HTMLButtonElement = document.querySelector('.widget_secondary-button')!
 const rateButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.widget_rate-group button')!
 const submitButton: HTMLButtonElement = document.querySelector('.widget_primary-button')!
 const emailInput: HTMLInputElement = document.querySelector('input[type="email"]')!
@@ -6,9 +9,11 @@ const buttonThumbsUpRadio: HTMLInputElement = document.querySelector('#button_th
 const buttonThumbsDownRadio: HTMLInputElement = document.querySelector('#button_thumbs-down')!
 const hiddenThumbsUpRadio: HTMLInputElement = document.querySelector('#radio_thumbs-up')!
 const hiddenThumbsDownRadio: HTMLInputElement = document.querySelector('#radio_thumbs-down')!
+const pageFooter: HTMLElement = document.querySelector('.p-footer')!
 let emailPopulated: boolean = false
 let textareaPopulated: boolean = false
 let rateButtonSelected: boolean = false
+let footerObserver: IntersectionObserver | undefined
 
 const handleRateButtonUncheckAll = () => {
 	// set boolean to false
@@ -42,10 +47,6 @@ const handleRateButtonCheck = (button: HTMLButtonElement) => {
 }
 
 const toggleWidget = () => {
-	const widgetWrapper = document.querySelector('.widget_wrapper')!
-	const topTab = document.querySelector('.widget_top-tab')!
-	const cancelButton = document.querySelector('.widget_secondary-button')!
-
 	topTab.addEventListener('click', () => {
 		widgetWrapper?.classList.toggle('-active');
 	})
@@ -112,7 +113,69 @@ const handleRateButtonClick = () => {
 	}
 }
 
-const handleForm = () => {
+// handles widget sitting behind, and sticking to the top of footer instead of the bottom of the viewport
+// const footerIntersectionObserver = () => {
+// 	if (footerObserver) {
+// 		footerObserver.disconnect()
+// 	}
+
+// 	footerObserver = new IntersectionObserver((entries) => {
+
+// 		for (const entry of entries) {
+// 			console.log(entry)
+// 			if (entry.isIntersecting) {
+// 				console.log(entry)
+// 				widgetWrapper.style.insetBlockEnd = '-309px';
+// 				pageFooter.style.position = 'relative';
+// 			} else {
+// 				widgetWrapper.style.removeProperty('inset-block-end')
+// 			}
+// 		}
+// 	}, {
+// 		// rootMargin: `0% 0px -${visualViewport.height - 60}px`,
+// 		threshold: 0,
+// 	})
+
+// 	footerObserver.observe(pageFooter)
+// }
+
+
+/////
+
+// sets icon panel distance from top dynamically so it always sits underneath the icon search even though it is position: fixed
+// to be run on scroll
+const setWidgetPositionOnFooter = () => {
+	let footerHeight: number = pageFooter.offsetHeight;
+	console.log(pageFooter.getBoundingClientRect())
+	let footerRect: number = pageFooter.getBoundingClientRect().y;
+	let widgetOffset: number  = 473;
+	let offset: number = footerRect - widgetOffset;
+    widgetWrapper.style.insetBlockEnd = `-${offset}px`;
+}
+
+// set intersection observer on header so the onscroll event listener is only running for the length of the header scroll
+const options = {
+	rootMargin: '0px',
+	threshold: 0,
+}
+
+const intersectCallback = (entries: any[]) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			window.addEventListener('scroll', setWidgetPositionOnFooter)
+		} else {
+			window.removeEventListener('scroll', setWidgetPositionOnFooter)
+		}
+	})
+}
+
+footerObserver = new IntersectionObserver(intersectCallback, options)
+
+footerObserver.observe(pageFooter)
+
+//////
+
+const handleFormSubmit = () => {
 	const form = document.querySelector('form')
 }
 
@@ -120,3 +183,5 @@ const handleForm = () => {
 toggleWidget()
 handleRateButtonClick()
 isFormPopulated()
+
+// footerIntersectionObserver()
