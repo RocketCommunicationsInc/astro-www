542b15c9c9df70c9a98646a3139fa2556eff30ba
Kiley <110477068+kiley-mitti@users.noreply.github.com>
2023-04-05 13:18:20 -0400

    Update Google Calendar Events Date Logic (#161)
    * Update Community Events API (#162)
    
    * Update Community Events API
    
    * Format TSConfig
    
    * Remove debugging event
    
    * Update Calendar Types
    
    * Remove settings.json change
    
    * Restore index.astro
    
    * minor tweaks
    
    ---------
    
    Co-authored-by: Kiley Mitti <kiley.mitti@rocketcom.com>
    
    * fix package
    
    ---------
    
    Co-authored-by: Jonathan Neal <jonathantneal@hotmail.com>

--- src/components/home/community/home-community-events.eventToggleHandler.js
+++ src/components/home/community/home-community-events.eventToggleHandler.js
@@ -15,10 +15,12 @@
 
 	// handle toggle events
 	eventElement.addEventListener('click', event => {
-		//if you're clicking inside the event details don't activate close
-		if(event.target.closest('.p-community-event-details') !== null) return;
+		// if you're clicking inside the event details don't activate close
+		if (event.target.closest('.p-community-event-details') !== null) return;
 
-		if(eventElement.classList.contains('--closed')) eventDetailsElement.style.setProperty('--content-height', eventDetailsElement.scrollHeight + 32 + 'px')
+		if (!eventDetailsElement) return;
+
+		if (eventElement.classList.contains('--closed')) eventDetailsElement.style.setProperty('--content-height', eventDetailsElement.scrollHeight + 32 + 'px')
 
 		eventElement.classList.toggle('--closed')
 		eventElement.classList.toggle('--open')
@@ -28,8 +30,8 @@
 
 	// telemetry: user opens the community event details
 	eventElement.addEventListener('click', (event) => {
-		//if you're clicking inside the event details don't activate close
-		if(event.target.closest('.p-community-event-details') !== null) return;
+		// if you're clicking inside the event details don't activate close
+		if (event.target.closest('.p-community-event-details') !== null) return;
 		if (!eventElement.classList.contains('--open')) return
 
 		gtag('event', 'open_community_event_details')
