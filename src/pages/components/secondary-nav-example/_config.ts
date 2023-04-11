f13220ed71d7ea4134fe2c97d6329203f162a9fd
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-04-11 10:30:43 -0400

    fix example

--- src/pages/components/secondary-nav-example/_config.ts
+++
@@ -1,9 +0,0 @@
-import * as overviewData from './index.md'
-import * as specsData from './specs.md'
-// import * as usageData from './usage.md'
-
-export default [
-	overviewData,
-	specsData,
-	// usageData,
-]
