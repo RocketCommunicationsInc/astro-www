9c94af568c7daf5b84ff4c15c4a670b07edda7ab
Jonathan Neal <jonathantneal@hotmail.com>
2023-04-19 14:07:23 -0400

    Fixes applied by npm checks (#193)
    * Fixes applied by npm checks
    
    * Remove unused code

--- src/components/asset-status/asset-status.keyToggleHandler.js
+++ src/components/asset-status/asset-status.keyToggleHandler.js
@@ -3,24 +3,22 @@
 
 	/** Asset Status Mobile Status Key */
 	const eventElement = scriptEl.previousSibling
-	const toggle = eventElement.querySelector('.-toggle')
 
-	/*closed by default*/
+	/* closed by default */
 	eventElement.classList.add('--closed')
 
-	window.addEventListener('click', (event)=>{
-		//if the click is in the key do nothing
-		if(event.target.closest('.-key') !== null) return;
+	window.addEventListener('click', (event) => {
+		// if the click is in the key do nothing
+		if (event.target.closest('.-key') !== null) return
 
-		//if the click is not on the key make sure it closes
+		// if the click is not on the key make sure it closes
 		if (event.target.closest('.-toggle') === null) {
 			eventElement.classList.remove('--open')
 			eventElement.classList.add('--closed')
-			return;
+			return
 		}
-		//otherwise toggle as normal
+		// otherwise toggle as normal
 		eventElement.classList.toggle('--closed')
 		eventElement.classList.toggle('--open')
 	}, true)
-
 }
