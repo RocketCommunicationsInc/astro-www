520cc467c839f124ce59729a58e3dc865efa0187
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-04-21 10:26:41 -0400

    slideshow fix
    Co-authored-by: Frances Morrison <Frances.Morrison87@gmail.com>

--- src/components/slideshow/slideshow.ts
+++ src/components/slideshow/slideshow.ts
@@ -32,8 +32,8 @@ export class SlideshowElement extends HTMLElement {
 			}
 		}
 
-		const handleScrollButtonClick = (event: PointerEvent & { target: HTMLButtonElement }) => {
-			let button = event.target
+		const handleScrollButtonClick = (event: Event) => {
+			let button = event.target as HTMLElement
 
 			switch (true) {
 				case prev.contains(button): {
@@ -49,7 +49,8 @@ export class SlideshowElement extends HTMLElement {
 		}
 
 		slot.addEventListener('scroll', updateScrollButtons, { passive: true })
-		host.addEventListener('click', handleScrollButtonClick, { passive: true })
+		next.addEventListener('click', handleScrollButtonClick, { passive: true })
+		prev.addEventListener('click', handleScrollButtonClick, { passive: true })
 
 		updateScrollButtons()
 	}
