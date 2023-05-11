b798431dcd98928beb7d2eec363b52351cb19089
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-05-11 09:40:30 -0400

    more logs

--- src/components/component-playground/Control/Slots.ts
+++ src/components/component-playground/Control/Slots.ts
@@ -223,12 +223,12 @@ addEventListener('load', () => {
 	const slots = getSlots()
 	$slots = [ ...slots ] as any[]
 	getEnabledSlots()
-	console.log($tag, $target, $canvas, $actualSlots, $slots)
+	console.log('load!', $tag, $target, $canvas, $actualSlots, $slots)
 })
 
 // get new variant and change slot disabled/enabled when variant changes
 addEventListener('reset', () => {
 	$target = $canvas.querySelector($tag)
 	getEnabledSlots()
-	console.log($tag, $target, $canvas, $actualSlots, $slots)
+	console.log('reset!', $tag, $target, $canvas, $actualSlots, $slots)
 })
