a260cf784b95274d3021ea4be450099b40e0b797
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-05-19 13:52:54 -0400

    initial tailwind config

--- astro.config.js
+++ astro.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config'
-
 import astroPlugins from './.vscode/astro-plugins.js'
 import astroSitemapIntegration from '@astrojs/sitemap'
 import astroWebComponentPolyfills from '@astropub/webcomponent-polyfills'
+import tailwind from '@astrojs/tailwind'
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://www.astrouxds.com/',
-	trailingSlash: 'always',
-	server: {
-		host: true,
-		port: 3000,
-	},
-	integrations: [
-		astroPlugins(),
-		astroSitemapIntegration(),
-		astroWebComponentPolyfills(),
-	],
+  site: 'https://www.astrouxds.com/',
+  trailingSlash: 'always',
+  server: {
+    host: true,
+    port: 3000
+  },
+  integrations: [ astroPlugins(), astroSitemapIntegration(), astroWebComponentPolyfills(), tailwind() ]
 })
