0178fe4c010049b186a81d52596db488039173e7
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-07-14 14:14:03 -0400

    comment these back out

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-<reference types="../.vscode/tsconfig.d.ts" />
-<reference types="types-object" />
-<reference types="@typescript/lib-dom" />
-<reference types="astro/client" />
+// <reference types="../.vscode/tsconfig.d.ts" />
+// <reference types="types-object" />
+// <reference types="@typescript/lib-dom" />
+// <reference types="astro/client" />
