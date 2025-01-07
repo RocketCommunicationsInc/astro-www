c5077c3912b89d3a311eaf1123304a498257bf35
Frances Morrison <frances.morrison87@gmail.com>
2025-01-07 10:24:44 -0500

    feat: adding mutation observer to show/hide letter nav buttons based on results

--- src/components/glossary-search/parts/glossary-nav.js
+++ src/components/glossary-search/parts/glossary-nav.js
@@ -1,18 +1,47 @@
- // Find our component DOM on the page.
- const navButtons = document.querySelectorAll('.p-glossary-nav-button')
+{
+	// Find our component DOM on the page.
+	const navButtons = document.querySelectorAll('.p-glossary-nav-button')
 
- // Add event listeners to fire confetti when a button is clicked.
- navButtons.forEach((button) => {
-   button.addEventListener('click', (event) => handleClick(event))
- })
+	// Add event listeners to fire confetti when a button is clicked.
+	navButtons.forEach((button) => {
+		button.addEventListener('click', (event) => handleClick(event))
+	})
 
-const handleClick = (event) => {
-	navButtons.forEach(button => button.classList.remove('selected'))
+	// mutation observer for filtered or search changes in the group header matches
+	const callback = (mutationsList, observer) => {
+		const visibleLetters = [
+			...document.querySelectorAll(
+				'.p-glossary-item-groups .-group:not(.nomatch)'
+			),
+		].map((group) => group.querySelector('.-group-heading').id)
 
-	const button = event.target
-	const { height } = document.querySelector('.p-glossary-search').getBoundingClientRect()
-	const { x, y } = document.querySelector(`h2#${button.id}`).getBoundingClientRect()
+		navButtons.forEach(button => {
+			if (!visibleLetters.includes(button.id)) {
+				button.classList.add('hidden')
+			} else {
+				button.classList.remove('hidden')
+			}
+		})
+	}
 
-	button.classList.add('selected')
-	window.scrollBy(x, y - height)
+	const observer = new MutationObserver(callback)
+	const targetNode = document.querySelector('.p-glossary-item-groups')
+	const config = { attributes: true, childList: false, subtree: true }
+
+	observer.observe(targetNode, config)
+
+	const handleClick = (event) => {
+		navButtons.forEach((button) => button.classList.remove('selected'))
+
+		const button = event.target
+		const { height } = document
+			.querySelector('.p-glossary-search')
+			.getBoundingClientRect()
+		const { x, y } = document
+			.querySelector(`h2#${button.id}`)
+			.getBoundingClientRect()
+
+		button.classList.add('selected')
+		window.scrollBy(x, y - height)
+	}
 }
