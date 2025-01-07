b91ecf53f65939f5226582fa939fcb821656c930
Frances Morrison <frances.morrison87@gmail.com>
2025-01-07 06:42:08 -0500

    chore: capitalizing first letter of category

--- src/components/glossary-search/parts/glossary-search-categories.js
+++ src/components/glossary-search/parts/glossary-search-categories.js
@@ -1,15 +1,39 @@
 {
 	const select = document.currentScript.previousSibling
 
-	const optionsNotUnique = [ ...document.querySelectorAll('.glossary-categories') ].map((categoryElement) => (
-		categoryElement.getAttribute('data-categories').split(',')
-	)).flat()
+	// util function
+	const capitalizeFirstLetter = (word) => {
+		const firstLetterCap = word.charAt(0).toUpperCase()
+		const remainingLetters = word.slice(1)
+		return firstLetterCap + remainingLetters
+	}
 
-	const options = [ ...new Set(optionsNotUnique) ]
-	options.forEach(option => {
+	// grab all category options and flatten into an array
+	const optionsNotUnique = [
+		...document.querySelectorAll('.glossary-categories'),
+	]
+		.map((categoryElement) =>
+			categoryElement.getAttribute('data-categories').split(',')
+		)
+		.flat()
+
+	// use a Set to remove dupes, map through and capitalize first letter
+	const options = [ ...new Set(optionsNotUnique) ].map((opt) => {
+		if (opt.includes('_')) {
+			return opt
+				.split('_')
+				.map((word) => capitalizeFirstLetter(word))
+				.join(' ')
+		} else {
+			return capitalizeFirstLetter(opt)
+		}
+	})
+
+	// append each option value to select element
+	options.forEach((option) => {
 		const newOption = document.createElement('option')
 		newOption.value = option
-		newOption.textContent = option.replaceAll('_', ' ')
+		newOption.textContent = option
 		select.appendChild(newOption)
 	})
 }
