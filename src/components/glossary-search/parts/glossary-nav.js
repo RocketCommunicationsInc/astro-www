2ccc3abf2c8939d8311b872bef68e85bebdc1249
Frances Morrison <frances.morrison@rocketcom.com>
2025-01-08 15:27:41 -0500

    feat(glossary): center glossary nav buttons, mobile fixes, mobile nav dropdown (#393)
    * chore: adding in select menu for glossary nav on mobile
    
    * fix: mobile scrollTo and scrollBy fixes

--- src/components/glossary-search/parts/glossary-nav.js
+++ src/components/glossary-search/parts/glossary-nav.js
@@ -1,10 +1,15 @@
 {
 	const navButtons = document.querySelectorAll('.p-glossary-nav-button')
+	const navSelect = document.querySelector('.p-glossary-nav-select select')
+	const navOptions = navSelect.querySelectorAll('option')
 
-	// Add event listeners to fire confetti when a button is clicked.
+	// Add event listeners for nav buttons
 	navButtons.forEach((button) => {
-		button.addEventListener('click', (event) => handleClick(event))
+		button.addEventListener('click', (event) => handleNavigate(event))
 	})
+
+	// Add event listener for nav select
+	navSelect.addEventListener('change', (event) => handleNavigate(event))
 
 	// mutation observer for filtered or search changes in the group header matches
 	const callback = (mutationsList, observer) => {
@@ -16,13 +21,25 @@
 
 		navButtons.forEach((button) => {
 			// remove selected state
-			if (button.classList.contains('selected')) { button.classList.remove('selected') }
+			if (button.classList.contains('selected')) {
+				button.classList.remove('selected')
+			}
 
 			// hide all buttons that don't have visible group headings
 			if (!visibleLetters.includes(button.id)) {
 				button.classList.add('hidden')
 			} else {
 				button.classList.remove('hidden')
+			}
+		})
+
+		navSelect.value = 'all'
+		navOptions.forEach((option) => {
+			// hide all options that don't have visible group headings
+			if (!visibleLetters.includes(option.id)) {
+				option.classList.add('hidden')
+			} else {
+				option.classList.remove('hidden')
 			}
 		})
 	}
@@ -33,18 +50,34 @@
 
 	observer.observe(targetNode, config)
 
-	const handleClick = (event) => {
-		navButtons.forEach((button) => button.classList.remove('selected'))
+	const handleNavigate = (event) => {
+		const element = event.target
+		if (element instanceof HTMLButtonElement) {
+			navButtons.forEach((button) => button.classList.remove('selected'))
+			element.classList.add('selected')
+		}
 
-		const button = event.target
-		const { height } = document
+		const toolbarHeight = document
 			.querySelector('.p-glossary-search')
-			.getBoundingClientRect()
+			.getBoundingClientRect().height
+		const topNavHeight = document
+			.querySelector('nav.p-navigation')
+			.getBoundingClientRect().height
 		const { x, y } = document
-			.querySelector(`h2#${button.id}`)
+			.querySelector(
+				`h2#${
+					element instanceof HTMLButtonElement ? element.id : element.value
+				}`
+			)
 			.getBoundingClientRect()
 
-		button.classList.add('selected')
-		window.scrollBy(x, y - height)
+		// scroll to x, y coords minus height of sticky toolbar in desktop and toolbar plus top nav in mobile
+		window.scrollBy(
+			x,
+			y -
+				(window.visualViewport.width < 1024
+					? toolbarHeight + topNavHeight
+					: toolbarHeight)
+		)
 	}
 }
