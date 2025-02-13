7d76fa274d43b2a2bb4d5813cfec7228a38fb9fd
Frances Morrison <frances.morrison87@gmail.com>
2025-02-13 07:40:01 -0500

    wip: testing base url

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -1,15 +1,15 @@
 // STRIPE_SECRET for real
 const stripe = require('stripe')(process.env.STRIPE_SANDBOX_SECRET)
 // const baseUrl = process.env.DEPLOY_URL || process.env.BASE_URL || 'http://localhost:8888'
+const baseUrl = process.env.BASE_URL
 
-console.log('Environment Variables:', process.env)
 
-let baseUrl
-if (process.env.CONTEXT === 'deploy-preview') {
-	baseUrl = process.env.DEPLOY_PRIME_URL
-} else if (process.env.CONTEXT === 'production') { baseUrl = process.env.BASE_URL } else {
-	baseUrl = 'https://www.astrouxds.com'
-}
+// let baseUrl
+// if (process.env.CONTEXT === 'deploy-preview') {
+// 	baseUrl = process.env.DEPLOY_PRIME_URL
+// } else if (process.env.CONTEXT === 'production') { baseUrl = process.env.BASE_URL } else {
+// 	baseUrl = 'https://www.astrouxds.com'
+// }
 
 const PRODUCTS = {
 	'fds': {
