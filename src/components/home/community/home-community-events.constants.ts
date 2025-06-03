361ff4ccba26be4ad0a56ae0dce7cf2d85337379
Bryce Rothhammer <bryce.rothhammer@rocketcom.com>
2025-06-03 09:57:07 -0600

    ???

--- src/components/home/community/home-community-events.constants.ts
+++ src/components/home/community/home-community-events.constants.ts
@@ -1,36 +1,35 @@
 export const googleCalendarEventsApiUrl = 'https://www.googleapis.com/calendar/v3/calendars/c_c00bcad50f7ad0acd24c335ddf65e16efd7538c0dd57be307b40c1677feb2637@group.calendar.google.com/events'
 
-// export const googleCalendarEventsApiKey = process.env.GOOGLE_CALENDAR_API_KEY || ''
+export const googleCalendarEventsApiKey = process.env.GOOGLE_CALENDAR_API_KEY || ''
 
 /** Returns a list of fetched Google Calendar Events. */
 export const fetchGoogleCalendarEvents = async () => {
-	// /** Search Parameters attached to the Request URL */
-	// const searchParams = new URLSearchParams({
-	// 	key: googleCalendarEventsApiKey,
-	// 	maxResults: '6',
-	// 	orderBy: 'startTime',
-	// 	singleEvents: 'true',
-	// 	timeMin: new Date().toISOString(),
-	// })
+	/** Search Parameters attached to the Request URL */
+	const searchParams = new URLSearchParams({
+		key: googleCalendarEventsApiKey,
+		maxResults: '6',
+		orderBy: 'startTime',
+		singleEvents: 'true',
+		timeMin: new Date().toISOString(),
+	})
 
-	// /** Request URL */
-	// const request = googleCalendarEventsApiUrl + '?' + searchParams
+	/** Request URL */
+	const request = googleCalendarEventsApiUrl + '?' + searchParams
 
-	// /** Response from the Google Calendar API. */
-	// const response = await fetch(request, {
-	// 	cache: 'force-cache',
-	// 	headers: {
-	// 		Referer: 'https://www.astrouxds.com/',
-	// 	},
-	// })
+	/** Response from the Google Calendar API. */
+	const response = await fetch(request, {
+		cache: 'force-cache',
+		headers: {
+			Referer: 'https://www.astrouxds.com/',
+		},
+	})
 
-	// const { kind, items }: { kind: string, items: CalendarEvent[] } = await response.json()
+	const { kind, items }: { kind: string, items: CalendarEvent[] } = await response.json()
 
-	// // return an empty array if the response is not calendar events
-	// if (kind !== 'calendar#events' || !items) return []
+	// return an empty array if the response is not calendar events
+	if (kind !== 'calendar#events' || !items) return []
 
-	// return items
-	return []
+	return items
 }
 
 // rearrange date - Only triggers if the event is a google calendar All Day event.
