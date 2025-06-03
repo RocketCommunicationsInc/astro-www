e0b965c7f4e2732e1720b376b88b92b865cddb06
Bryce Rothhammer <bryce.rothhammer@rocketcom.com>
2025-06-03 10:14:09 -0600

    match env netlify name

--- src/components/home/community/home-community-events.constants.ts
+++ src/components/home/community/home-community-events.constants.ts
@@ -1,6 +1,6 @@
 export const googleCalendarEventsApiUrl = 'https://www.googleapis.com/calendar/v3/calendars/c_c00bcad50f7ad0acd24c335ddf65e16efd7538c0dd57be307b40c1677feb2637@group.calendar.google.com/events'
 
-export const googleCalendarEventsApiKey = process.env.GOOGLE_CALENDAR_API_KEY || ''
+export const googleCalendarEventsApiKey = process.env.GOOGLE_CALENDAR_EVENTS_API_KEY || ''
 
 /** Returns a list of fetched Google Calendar Events. */
 export const fetchGoogleCalendarEvents = async () => {
