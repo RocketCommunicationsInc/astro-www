866fbb32b64747541ea90992ba615c5bcf7eec7f
Frances Morrison <frances.morrison87@gmail.com>
2025-11-20 10:33:47 -0500

    fix: environment file

--- netlify/functions/config/environment.js
+++ netlify/functions/config/environment.js
@@ -106,58 +106,41 @@
  * @property {string} development
  */
 
-const isDevelopment = false
-const reviewAppUrl = 'https://astrouxds-ap-dev-enviro-szmhil.herokuapp.com/'
+export const isDevelopment = false
+export const reviewAppUrl = 'https://astrouxds-ap-dev-enviro-szmhil.herokuapp.com/'
 
-const API_URLS = {
+export const API_URLS = {
   production: 'https://astrouxds-api-196cde1c48d0.herokuapp.com',
   development: reviewAppUrl
 }
 
-const STRIPE_VARIABLES = {
-  development: {
-    publicKey: 'pk_test_51MIsfsCX2F0Knv6wJXtNKJI5mf5a4h9rkVb1TLm7rVeZCdM7MvHmwoHyQd5AqsQZRk48g0t3hYQHUbTFp4XD9FgN00mjvHNO9O',
+export const STRIPE_VARIABLES = {
+  production: {
+    publicKey: 'pk_live_51Q57PtCecnrjj3thBAsVNZ5R02YmZEg4kveWSD7ipTUEX0F7VKVdlTFT3UPHILBUskCmPnOWdCUlvuHp5LDuuGhV00X7Im0xoK',
     products: {
       'astro-toolkit-ppt': {
-        stripeProductId: 'prod_SLdY3pRKOo37hn',
+        stripeProductId: 'prod_RvMCdid2pZCbUf',
         stripePriceIds: {
-          'individual': 'price_1RQwHUCX2F0Knv6wHJ8f615k',
-          'team': 'price_1SUPgTCX2F0Knv6w3Gkbu2Mj',
-          'project': 'price_1SUPhxCX2F0Knv6wikxBiyCK'
-        },
-        tierDescriptions: {
-          'individual': 'Single user license',
-          'team': 'Team license for up to 10 users',
-          'project': 'Project-wide license'
+          'individual': 'price_1R1VU2Cecnrjj3thckjd6Qv4',
+					'team': 'price_1SUubOCecnrjj3thzmH4xK8G',
+					'project': 'price_1SUucHCecnrjj3th3izyyL2M',
         },
         name: 'Astro Toolkit PPT'
       }
     }
   },
-  production: {
-    publicKey: 'pk_live_51MIsfsCX2F0Knv6wdRx3v6YP9aT45lQVZXiHkFfD9VQrYe0u8MK4Rqn9wgKa8VH5zMXvgJDc1234567890',
+  development: {
+    publicKey: 'pk_test_51QCk6mCX2F0Knv6wLx3fRI5bIvIKHPa4LcZS2DU0aXMyalKcSsplszWAvftDIjSs072xOs5ZHN264qMnjjHi7Ml600FWdSVdql',
     products: {
       'astro-toolkit-ppt': {
-        stripeProductId: 'prod_PRODUCTION_ID',
+        stripeProductId: 'prod_SLdY3pRKOo37hn',
         stripePriceIds: {
-          'individual': 'price_PROD_INDIVIDUAL',
-          'team': 'price_PROD_TEAM',
-          'project': 'price_PROD_PROJECT'
-        },
-        tierDescriptions: {
-          'individual': 'Single user license',
-          'team': 'Team license for up to 10 users',
-          'project': 'Project-wide license'
-        },
+					'individual': 'price_1RQwHUCX2F0Knv6wHJ8f615k',
+					'team': 'price_1SUPgTCX2F0Knv6w3Gkbu2Mj',
+					'project': 'price_1SUPhxCX2F0Knv6wikxBiyCK'
+				},
         name: 'Astro Toolkit PPT'
       }
     }
   }
-}
-
-module.exports = {
-  STRIPE_VARIABLES,
-  isDevelopment,
-  reviewAppUrl,
-  API_URLS
 }
